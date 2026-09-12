64a209ec4275a1f1b6cfc940381873b31052e046
Jesús Emanuel <L26550308@chihuahua2.TECNM.MX>
2026-09-12 12:06:10 -0500

    Add files via upload

---
+++ EVA1_3_Salidas/src/eva1_3_salidas/EVA1_3_Salidas.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_salidas;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_3_Salidas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a Fundamentos de Programacion");
+        System.out.println("Jesús Emanuel Ramos España");
+        System.out.print("Mi calificacion sera");
+        System.out.println(" 85");
+        // TODO code application logic here
+    }
+    
+}

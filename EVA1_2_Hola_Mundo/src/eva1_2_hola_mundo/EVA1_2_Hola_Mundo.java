2c7ab35f8c14798fd9a05ee745f9e11a9ca0c9e7
Jesús Emanuel <L26550308@chihuahua2.TECNM.MX>
2026-09-12 12:05:21 -0500

    Add files via upload

---
+++ EVA1_2_Hola_Mundo/src/eva1_2_hola_mundo/EVA1_2_Hola_Mundo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_hola_mundo;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_2_Hola_Mundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hola Mundo ");
+        // TODO code application logic here
+    }
+    
+}

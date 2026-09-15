1c60ea8b2878df26b7171b468618f4b35998f630
Jesús Emanuel <L26550308@chihuahua2.TECNM.MX>
2026-09-14 19:53:32 -0500

    Add files via upload

---
+++ EVA1_9_OPERACIÓNES_CADENAS/src/eva1_9_operaciónes_cadenas/EVA1_9_OPERACIÓNES_CADENAS.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_9_operaciónes_cadenas;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_9_OPERACIÓNES_CADENAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //TAMBIEN TENEMOS OPERADORES PAR ALAS CADENAS
+        //No se pueden multiplicar, restar o dividir, solo sumar
+        String cade;
+        cade = "Hola" + " " + " Mundo ";
+        System.out.println(cade);
+        cade = "valor = " + 100;
+        System.out.println(cade);
+        cade = "Valores boolannea" + false;
+        System.out.println(cade);
+              
+    }
+    
+}

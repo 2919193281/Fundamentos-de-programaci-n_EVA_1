daf59e22838c90ad2220057b513c700ef9df5ab8
Jesús Emanuel <L26550308@chihuahua2.TECNM.MX>
2026-09-14 19:53:06 -0500

    Add files via upload

---
+++ EVA1_8_OPERACIÓNES2/src/eva1_8_operaciónes2/EVA1_8_OPERACIÓNES2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_operaciónes2;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_8_OPERACIÓNES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int resu, valo1 = 10, valo2 = 5; // Declaro e inicializando 
+        //Java por defecto incializa en automatico las variables
+        //numero en cero, bolñleanos en Falso
+        resu = 100 + valo1 * (valo1 / valo2);
+        System.out.print("Resultado = ");
+        System.out.println(resu);
+    }
+    
+}
